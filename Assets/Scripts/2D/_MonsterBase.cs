e75f46e0e99bafad5049068149043310bb741bba
SonghaCH <cwithus0002@gmail.com>
2026-07-10 16:27:25 +0900

    [1차빌드] 체크 아웃 최종본

--- Assets/Scripts/2D/_MonsterBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-[Obsolete("2D용 레거시", false)]
-public class _MonsterBase : MonoBehaviour
-{
-}
--- Assets/Scripts/Common/WorldManager.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class WorldManager : MonoBehaviour
-{
-    // 로직 컴포넌트를 등록하고 뺄 수 있도록 해둠
-    [SerializeField] private LogicBase CurrentLogicBase;
-}
