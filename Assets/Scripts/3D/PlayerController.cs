d053939c190b1acb00c5250aba034d90a0af7d5a
ClairvoyantHG <wjdgory@gmail.com>
2026-07-10 11:12:59 +0900

    [정해교] [Player] 플레이어 기본 이동, 마우스로 시야 이동 구현
    이후 MVVM으로 재구성할 예정

---
+++ Assets/Scripts/3D/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform playerCamera;
+
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private float _mouseSensitivity = 2f;
+    [SerializeField] private float _gravity = -10f;
+
+    private float _xRotation = 0f;
+    private Vector3 _velocity; 
+    private CharacterController _controller;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        LookAround();
+        MovePlayer();
+        ApplyGravity(); 
+    }
+
+    void LookAround()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * _mouseSensitivity;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
+
+        if (playerCamera != null)
+        {
+            playerCamera.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+        }
+
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        _controller.Move(move * _moveSpeed * Time.deltaTime);
+    }
+
+    void ApplyGravity()
+    {
+        if (_controller.isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        _velocity.y += _gravity * Time.deltaTime;
+
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+}
