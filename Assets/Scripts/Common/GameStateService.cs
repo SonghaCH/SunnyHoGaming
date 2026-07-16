7b233f53d71c85b3919af24301d921437758a9fb
ClairvoyantHG <wjdgory@gmail.com>
2026-07-16 10:53:01 +0900

    [정해교] [플레이 상태 관리] GameState 관리 MVVM 추가

---
+++ Assets/Scripts/Common/GameStateService.cs
@@ -0,0 +1,48 @@
+﻿public class GameStateService
+{
+    private GameStateViewModel _viewModel;
+
+    public GameStateService()
+    {
+        _viewModel = new GameStateViewModel();
+    }
+
+    public GameStateViewModel GetViewModel()
+    {
+        return _viewModel;
+    }
+
+    public void EnterTitleScreen()
+    {
+        _viewModel.CurrentGameState = GameState.Title;
+    }
+
+    public void PlayGame()
+    {
+        _viewModel.CurrentGameState = GameState.Playing;
+    }
+
+    public void PauseGame()
+    {
+        if (_viewModel.CurrentGameState == GameState.Playing)
+        {
+            _viewModel.CurrentGameState = GameState.Paused;
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (_viewModel.CurrentGameState == GameState.Paused)
+        {
+            _viewModel.CurrentGameState = GameState.Playing;
+        }
+    }
+
+    public void TriggerGameOver()
+    {
+        if (_viewModel.CurrentGameState != GameState.GameOver)
+        {
+            _viewModel.CurrentGameState = GameState.GameOver;
+        }
+    }
+}
