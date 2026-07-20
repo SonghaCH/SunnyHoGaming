12d754686a1df438e0d41dd529d7f8c4e80bc24a
강혜창 <a4076a4076@gmail.com>
2026-07-20 13:27:08 +0900

    [강혜창][Map] Fixer 팝업 띄워주는 코드 수정과 애니메이션 수정

--- Assets/Scripts/Fixer/FixerInteractController.cs
+++ Assets/Scripts/Fixer/FixerInteractController.cs
@@ -1,24 +1,37 @@
-﻿using UnityEngine;
+﻿using NUnit.Framework.Interfaces;
+using System;
+using UnityEngine;
 
 [RequireComponent(typeof(Collider), typeof(FixerViewModel))]
 public class FixerInteractController : MonoBehaviour
 {
     private FixerViewModel _viewModel;
+    private ObjectData _data;
+
 
     private void Awake()
     {
         _viewModel = GetComponent<FixerViewModel>();
+
+        string fixerId = gameObject.name.Replace("(Clone)", "").Trim();
+        _data = GameDataManager.Instance.GetObjectData(fixerId);
+        if ( _data == null )
+        {
+            Debug.LogError($"[FixerInteractControlle] 오브젝트가 할당되지 않았습니다.");
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            if (UserInputManager.instance != null)
+            UserInputManager.instance.OnInteractionKey += Interact;
+
+            var uiBase = UIManager.Instance.OpenUI(UIRootType.PopupUI, UIType.FPopupUI);
+            if (uiBase is FPopupUI fPopupUI)
             {
-                UserInputManager.instance.OnInteractionKey += Interact;
+                fPopupUI.SetInteractName(_data.Name);
             }
-            UIManager.Instance.OpenUI(UIRootType.PopupUI, UIType.FPopupUI);
         }
     }
 
@@ -26,11 +39,15 @@ public class FixerInteractController : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            if (UserInputManager.instance != null)
+            UserInputManager.instance.OnInteractionKey -= Interact;
+            UIManager.Instance.CloseFPopupUI();
+
+            if (!Enum.TryParse(_data.PopupType, out UIType popupUI))
             {
-                UserInputManager.instance.OnInteractionKey -= Interact;
+                Debug.LogError($"[MainController] '{gameObject.name}'의 PopupType '{_data.PopupType}'이 UIType에 없습니다.");
+                return;
             }
-            UIManager.Instance.CloseUI(UIRootType.PopupUI, UIType.FPopupUI);
+            UIManager.Instance.CloseUI(UIRootType.PopupUI, popupUI);
         }
     }
 
