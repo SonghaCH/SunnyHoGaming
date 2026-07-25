56f4207d5e4e74494f9603b6997d7492ae465dcb
강혜창 <a4076a4076@gmail.com>
2026-07-25 19:25:29 +0900

    [강혜창][UI] GameStsrtUI 프리팹 수정과 SkyBoxCamera 프리팹 추가

---
+++ Assets/Scripts/MapObject/SkyboxController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SkyboxController : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 1f; // 초당 회전 각도 (도)
+
+    private void Update()
+    {
+        if (RenderSettings.skybox == null)
+        {
+            return;
+        }
+
+        float currentRotation = RenderSettings.skybox.GetFloat("_Rotation");
+        currentRotation += _rotationSpeed * Time.deltaTime;
+
+        if (currentRotation > 360f)
+        {
+            currentRotation -= 360f;
+        }
+
+        RenderSettings.skybox.SetFloat("_Rotation", currentRotation);
+    }
+}
--- Assets/Scripts/UI/GameStartUI.cs
+++ Assets/Scripts/UI/GameStartUI.cs
@@ -8,15 +8,30 @@ public class GameStartUI : UIBase
     [SerializeField] private UIButton Btn_Start;
     [SerializeField] private UIButton Btn_Setting;
     [SerializeField] private UIButton Btn_Exit;
+    [SerializeField] private GameObject _skyboxCameraPrefab;
 
+    private GameObject _skyboxCameraInstance;
 
     private void OnEnable()
     {
+        _skyboxCameraInstance = Instantiate(_skyboxCameraPrefab);
+
         Btn_Start.BindOnClickButtonEvent(OnClick_Start);
         Btn_Setting.BindOnClickButtonEvent(OnClick_Setting);
         Btn_Exit.BindOnClickButtonEvent(OnClick_Exit);
     }
 
+    private void OnDisable()
+    {
+        Btn_Start.UnBindAllOnClickButtonEvent();
+        Btn_Setting.UnBindAllOnClickButtonEvent();
+        Btn_Exit.UnBindAllOnClickButtonEvent();
+
+        if (_skyboxCameraInstance != null)
+        {
+            Destroy(_skyboxCameraInstance);
+        }
+    }
 
     private void OnClick_Start()
     {
@@ -30,10 +45,9 @@ public class GameStartUI : UIBase
         UIManager.Instance.OpenSettingPopupUI();
         Debug.Log("세팅 버튼~");
     }
-   
+
     private void OnClick_Exit()
     {
         Application.Quit();
     }
-
 }
