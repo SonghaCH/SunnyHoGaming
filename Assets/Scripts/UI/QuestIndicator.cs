caf9237011da23357c0c725fca12872e5bfe388e
ClairvoyantHG <wjdgory@gmail.com>
2026-07-26 02:46:27 +0900

    [정해교] [퀘스트] 임시 퀘스트 마커 추가

---
+++ Assets/Scripts/UI/QuestIndicator.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class QuestIndicator : MonoBehaviour
+{
+    [SerializeField] private RectTransform _indicatorRect;
+
+    [SerializeField] private Transform _questTarget;
+
+    [SerializeField] private Camera _mainCamera;
+
+    private float _edgePadding = 50.0f;
+
+    private void Awake()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (_questTarget == null)
+        {
+            return;
+        }
+
+        if (_indicatorRect == null)
+        {
+            return;
+        }
+
+        UpdateIndicatorPosition();
+    }
+
+    private void UpdateIndicatorPosition()
+    {
+        Vector3 screenPos = _mainCamera.WorldToScreenPoint(_questTarget.position);
+
+        bool isBehind = screenPos.z < 0.0f;
+
+        float screenWidthHalf = Screen.width * 0.5f;
+        float screenHeightHalf = Screen.height * 0.5f;
+        Vector3 screenCenter = new Vector3(screenWidthHalf, screenHeightHalf, 0.0f);
+
+        Vector3 targetPos = screenPos - screenCenter;
+
+        if (isBehind == true)
+        {
+            targetPos = -targetPos;
+        }
+
+        float maxX = screenWidthHalf - _edgePadding;
+        float maxY = screenHeightHalf - _edgePadding;
+
+        float absX = Mathf.Abs(targetPos.x);
+        float absY = Mathf.Abs(targetPos.y);
+
+        if (absX < 0.001f)
+        {
+            absX = 0.001f;
+        }
+
+        if (absY < 0.001f)
+        {
+            absY = 0.001f;
+        }
+
+        float ratioX = maxX / absX;
+        float ratioY = maxY / absY;
+
+        float minRatio = Mathf.Min(ratioX, ratioY);
+
+        if (minRatio < 1.0f || isBehind == true)
+        {
+            targetPos.x = targetPos.x * minRatio;
+            targetPos.y = targetPos.y * minRatio;
+        }
+
+        _indicatorRect.position = screenCenter + targetPos;
+    }
+}
