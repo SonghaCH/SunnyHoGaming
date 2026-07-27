a0480d0b786555bbb01d8e1306340ceff0a63087
ClairvoyantHG <wjdgory@gmail.com>
2026-07-27 11:19:23 +0900

    [정해교] [퀘스트] 퀘스트 마커 생성 로직 추가

--- Assets/Scripts/Work/RepairStation.cs
+++ Assets/Scripts/Work/RepairStation.cs
@@ -1,15 +1,47 @@
-﻿using UnityEngine;
+﻿using System.ComponentModel;
+using UnityEngine;
 
 public class RepairStation : WorkStation
 {
     public float DecayPerSecond = 2.0f;
+    private TimeViewModel _timeViewModel;
 
     private void Start()
     {
         MaxGauge = 100f;
         CurrentGauge = 100f;
+
+        if(UIManager.Instance != null)
+        {
+            UIManager.Instance.AddQuestTargetMarker(transform);
+        }
+
+        BindTimeViewModel();
     }
 
+    private void BindTimeViewModel()
+    {
+        if (_timeViewModel == null && NetworkManager.Inst != null && NetworkManager.Inst.TimeService != null)
+        {
+            _timeViewModel = NetworkManager.Inst.TimeService.GetViewModel();
+            if (_timeViewModel != null)
+            {
+                _timeViewModel.PropertyChanged -= OnPropertyChanged;
+                _timeViewModel.PropertyChanged += OnPropertyChanged;
+            }
+        }
+    }
+
+    private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == nameof(TimeViewModel.CurrentDay))
+        {
+            if (UIManager.Instance != null)
+            {
+                UIManager.Instance.AddQuestTargetMarker(transform);
+            }
+        }
+    }
     private void Update()
     {
         if (MaxGauge > 0 && CurrentGauge > 0)
