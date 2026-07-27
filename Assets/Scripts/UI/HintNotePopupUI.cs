32b72e87fd23ad466e90ed3939171c898c50392a
정명진 <audwls3159@gmail.com>
2026-07-27 16:11:05 +0900

    [정명진][UI] 힌트 노트 UI 스크립트 수정

--- Assets/Scripts/UI/HintNotePopupUI.cs
+++ Assets/Scripts/UI/HintNotePopupUI.cs
@@ -12,7 +12,7 @@ public class HintNotePopupUI : UIBase
 
     private void OnClick_Close()
     {
-        UIManager.Instance.CloseHiddenNotePopupUI();
+        UIManager.Instance.CloseHintNotePopupUI();
     }
 
 }
